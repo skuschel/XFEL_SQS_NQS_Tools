417fc72cbeeec4370d10c72ccafabe8a77a886ac
Stephan Kuschel <stephan.kuschel@gmail.com>
2019-05-01 11:00:12 +0200

    add setup.py and refactor project to become one package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Stephan Kuschel, 2019
+
+from setuptools import setup
+
+setup(name='xfelmay2019',
+      version='0.0.0',
+      include_package_data=True,
+      url='https://github.com/skuschel/XFELMay2019',
+      install_requires=['numpy>1.8'])
+      
---
+++ xfelmay2019/__init__.py
@@ -0,0 +1,10 @@
+'''
+The python package initally developed for the data analysis on the
+EuropeanXFEL in Hamburg in May 2019.
+
+see https://github.com/skuschel/XFELMay2019
+'''
+
+from .helper import *
+from . import generatorpipeline 
+
