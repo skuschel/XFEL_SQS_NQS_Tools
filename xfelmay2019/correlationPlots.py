19be9eaf65a66956d7ff20333d3a2a44529a5f65
Catherine Saladrigas <catsalad@max-exfl014.desy.de>
2019-05-10 02:09:44 +0200

    catsalads changes

--- xfelmay2019/correlationPlots.py
+++ xfelmay2019/correlationPlots.py
@@ -5,7 +5,7 @@ import pyqtgraph as pg
 import karabo_bridge as kb
 import karabo_data as kd
 
-def correlationPlots(run, Path, roi, offset = 0):
+def correlationPlots(run, Path, roi1, offset):
     '''
     outputs a scatter plot correlating FEL pulse intensity to light peak
     inputs
@@ -21,25 +21,44 @@ def correlationPlots(run, Path, roi, offset = 0):
 
     pixels, tofs, trainIds = xfel.getRunTOF( run, Path, tofrange=(265000,285000) )
     pulse_E = np.asarray(runData.get_array('SA3_XTD10_XGM/XGM/DOOCS:output','data.intensityTD' ))
+    scatImagesini = np.asarray(runData.get_array('SQS_DPU_LIC/CAM/YAG_UPSTR:daqOutput', 'data.image.pixels'))
+    
+    integratedROI1s = []
 
-    integratedROIs = []
     pulseEnergies = []
-    for ind, elem in enumerate(trainIds):
+    if offset > 0:
+        for ind, elem in enumerate(trainIds[0:len(trainIds) - offset]):
+            train_pos=np.where(trainIds==elem)[0][0]
+            temp_E=pulse_E[1,:]
+            pos=max(enumerate(temp_E), key=(lambda a: a[1]))
+            run_E=pulse_E[:,pos[0]]
+            train_E = run_E[train_pos+offset]
+            pulseEnergies.append(train_E )
 
-        train_pos=np.where(trainIds==elem)[0][0]
+            integratedROI1s.append(np.sum((tofs[train_pos])[(roi1[0]<pixels)&(pixels<roi1[1])]))
+    else:
+            
+        for ind, elem in enumerate(trainIds):
 
-        temp_E=pulse_E[1,:]
-        pos=max(enumerate(temp_E), key=(lambda a: a[1]))
-        run_E=pulse_E[:,pos[0]]
+            train_pos=np.where(trainIds==elem)[0][0]
+            #print (train_pos) 
 
-        train_E = run_E[train_pos]
-        pulseEnergies.append(train_E + offset)
-        integratedROIs.append(np.sum((tofs[train_pos])[(roi[0]<pixels)&(pixels<roi[1])]))
-        
-    plt.scatter(pulseEnergies, integratedROIs)
+            temp_E=pulse_E[1,:]
+            #print (temp_E)
+
+            pos=max(enumerate(temp_E), key=(lambda a: a[1]))
+            run_E=pulse_E[:,pos[0]]
+            #print (run_E)
+
+            train_E = run_E[train_pos+offset]
+            pulseEnergies.append(train_E )
+
+            integratedROI1s.append(np.sum((tofs[train_pos])[(roi1[0]<pixels)&(pixels<roi1[1])]))
+
+    
+    plt.scatter(pulseEnergies, integratedROI1s)
     plt.xlabel('pulse energy')
     plt.ylabel('integrated ROI from TOF')
     
-    corr = np.corrcoef(pulseEnergies, integratedROIs)
-    
     return
+
