49b341d6e7017c4a064f8401d041351b6988bbcd
Adam Simon Chatterley <aschatterley@gmail.com>
2019-05-24 14:33:27 +0200

    less broken

--- sqs_nqs_tools/online/bufferPlots.py
+++ sqs_nqs_tools/online/bufferPlots.py
@@ -4,27 +4,27 @@ import pyqtgraph as pg
 
 class ImBufferPlotter():
     def __init__(self, length):
-     '''
-    creates a figure with subplots, ready to display a buffer full of images
-    this does not give you the color bars. Good luck adding them
-    
-    remember to keep the fig output in memory, or it closes
-    
-    Input:
-        length of buffer
-    Output:
-        list of imageItems, the graphicsWindow
-    '''
-    self.fig = pg.GraphicsWindow()
-    self.views = []
-    for i in range(length):
-        fakeimgdata  = np.random.rand(100,100)
-        views.append(pg.ImageItem())
-        v = fig.addViewBox(row = 0, col = i)
-        v.addItem(views[-1])
-        views[-1].setImage(fakeimgdata)
+        '''
+        creates a figure with subplots, ready to display a buffer full of images
+        this does not give you the color bars. Good luck adding them
+        
+        remember to keep the fig output in memory, or it closes
+        
+        Input:
+            length of buffer
+        Output:
+            list of imageItems, the graphicsWindow
+        '''
+        self.fig = pg.GraphicsWindow()
+        self.views = []
+        for i in range(length):
+            fakeimgdata  = np.random.rand(100,100)
+            self.views.append(pg.ImageItem())
+            v = self.fig.addViewBox(row = 0, col = i)
+            v.addItem(self.views[-1])
+            self.views[-1].setImage(fakeimgdata)
 
-    def plotImBuffer(buf):
+    def plotImBuffer(self, buf):
         '''
         plot all the images in a buffer into this figure
         '''
