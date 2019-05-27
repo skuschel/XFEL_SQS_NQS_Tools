7753cdf2165fcad2cab506e97929682f0d9fb371
Bjoern Senfftleben <bjoern.senffteben@mbi-berlin.de>
2019-05-27 23:08:12 +0200

    tidy up bokeh test script

--- scripts/d-bjoerns_testy_bokeh_live.py
+++ scripts/d-bjoerns_testy_bokeh_live.py
@@ -1,3 +1,4 @@
+# IMPORT MODULES
 import time
 import numpy as np
 import holoviews as hv
@@ -10,6 +11,7 @@ import holoviews.plotting.bokeh
 from tornado import gen
 
 from bokeh.plotting import figure, curdoc
+from bokeh.layouts import column, row
 from functools import partial
 
 from threading import Thread
@@ -17,113 +19,136 @@ from threading import Thread
 import sqs_nqs_tools.online as online
 import sqs_nqs_tools as tools
 
-
-
-
-
+# MODULE CONFIGS
 hv.extension('bokeh')
-renderer = hv.renderer('bokeh')
+renderer = hv.renderer('bokeh')  # renderer to convert objects from holoviews to bokeh
+renderer = renderer.instance(mode="server")
 hv.output(dpi=300, size=100)
+doc = curdoc()  # DOC for Bokeh Objects
 
-#default Data
-N=400000
-N_datapts = N
-x = np.arange(N)
-y = np.random.rand(N)
+# DATA SOURCE
+#source = 'tcp://10.253.0.142:6666'  # LIVE
+source = 'tcp://127.0.0.1:8011' # emulated live
 
-start_tof = 130000
-end_tof = start_tof+N_datapts
-x_tof = np.arange(start_tof,end_tof)
+# DATA CONFIG
+N_datapts = 400000 # total number of TOF datapoints that are visualized
+start_tof = 130000 # index of first TOF datapoint considered
+## yielded config values
+end_tof = start_tof+N_datapts # index of last TOF datapoint considered
+x_tof = np.arange(start_tof,end_tof) # x-axis for tof data points
 
-pipe2 = Pipe(data=[])
-TOF_dmap = hv.DynamicMap(hv.Curve, streams=[pipe2])
-TOF_dmap2 = hv.DynamicMap(hv.Scatter, streams=[pipe2])
-
-TOF_dmap_opt = datashade(TOF_dmap, streams=[PlotSize, RangeXY], dynamic=True, cmap = ['blue'])
-TOF_dmap_opt2 = datashade(TOF_dmap2, streams=[PlotSize, RangeXY], dynamic=True, cmap = ['red'])
-layout = TOF_dmap_opt.opts(width=1500,height=800,ylim=(-500, 40),xlim=(130000,130000+N_datapts)) + TOF_dmap_opt2.opts(width=400,height=400,ylim=(-500, 40),xlim=(130000,130000+N_datapts))
-
-doc = curdoc()
-
+# Data handling functions
 @gen.coroutine
 def update_pipe(x,y):
-    pipe2.send((x,y))
+    _pipe__TOF_single.send((x,y))
 
 @online.pipeline
-def foldTofs(d):
+def processTofs(d):
     '''
-    fold all the tofs from a train into a singal tof
+    process tofs in pipeline
     '''
-    
-    d['tof'] = d['tof'][start_tof:end_tof]
-    d['x_tof'] = x_tof
+    d['tof'] = d['tof'][start_tof:end_tof] # cut out index range that we are interested in
+    d['x_tof'] = x_tof # add values for x axis
     return d
 
-def makeBigData():
-    source = 'tcp://10.253.0.142:6666'
-    source = 'tcp://127.0.0.1:8011'
-    print("Source: "+ source)
-    print("Build Pipeline: Start")
-    ds = online.servedata(source) #get the datastream
-    ds = online.getTof(ds) #get the tofs
-    ds = foldTofs(ds)
-    print("get train id from digitzer")
-    ds = online.getSomeDetector(ds, name='tid', spec0='SQS_DIGITIZER_UTC1/ADC/1:network', spec1='digitizers.trainId')
-    print("Build Pipeline: Done")
-    
-    # initialize variables for performance monitoring
-    t_start = time.time()
-    for_loop_step_dur = 0
-    n=-1
-    freq_avg = 0
-    dt_avg = 0
-    trainId = 0
-    trainId_old = -1
-    skip_count = 0
-    
-    for data in ds:
-        # performance monitor - frequency of displaying data + loop duration
-        n+=1
-        dt = (time.time()-t_start)
-        t_start = time.time()
-        freq = 1/dt
-        if n>0:
-            dt_avg = (dt_avg * (n-1) + dt) / n
-            freq_avg = 1/dt_avg
-            loop_classification_percent = for_loop_step_dur/0.1*100
+class performanceMonitor():
+    def __init__(self):
+        import time
+        self.t_start = time.time()
+        self.t_start_loop = self.t_start
+        self.for_loop_step_dur = 0
+        self.n=-1
+        self.freq_avg = 0
+        self.dt_avg = 0
+        self.trainId = 0
+        self.trainId_old = -1
+        self.skip_count = 0
+        
+    def iteration(self):
+        self.n+=1
+        self.dt = (time.time()-self.t_start)
+        self.t_start = time.time()
+        freq = 1/self.dt
+        if self.n>0:
+            self.dt_avg = (self.dt_avg * (self.n-1) + self.dt) / self.n
+            freq_avg = 1/self.dt_avg
+            loop_classification_percent = self.for_loop_step_dur/0.1*100
             if loop_classification_percent < 100:
                 loop_classification_msg="OK"
             else:
                 loop_classification_msg="TOO LONG!!!"
-            print("Frequency: "+str(round(freq_avg,1)) +" Hz  |  skipped: "+str(skip_count)+" ( "+str(round(skip_count/n*100,1))+" %)  |  n: "+str(n)+"/"+str(trainId)+"  |  Loop benchmark: "+str(round(loop_classification_percent,1))+ " % (OK if <100%) - "+loop_classification_msg) 
+            print("Frequency: "+str(round(freq_avg,1)) +" Hz  |  skipped: "+str(self.skip_count)+" ( "+str(round(self.skip_count/self.n*100,1))+" %)  |  n: "+str(self.n)+"/"+str(self.trainId)+"  |  Loop benchmark: "+str(round(loop_classification_percent,1))+ " % (OK if <100%) - "+loop_classification_msg) 
+        self.t_start_loop = time.time()
         
-        #TOF
-        t_start_loop = time.time()
+    def update_trainId(self,tid):
+        self.trainId_old = self.trainId
+        self.trainId = tid
+        if self.n == 0:
+            self.trainId_old = str(int(tid) -1)
+        if int(self.trainId) - int(self.trainId_old) is not 1:
+            self.skip_count +=1
+            
+    def time_for_loop_step(self):
+        self.for_loop_step_dur = time.time()-self.t_start_loop
+
+def makeDatastreamPipeline(source):
+    ds = online.servedata(source) #get the datastream
+    ds = online.getTof(ds) #get the tofs
+    ds = processTofs(ds) #treat the tofs
+    ds = online.getSomeDetector(ds, name='tid', spec0='SQS_DIGITIZER_UTC1/ADC/1:network', spec1='digitizers.trainId') #get current trainids from digitizer property
+    return ds
+
+def makeBigData():
+    print("Source: "+ source) # print source set for data
+    
+    # Setup Data Stream Pipeline
+    ds = makeDatastreamPipeline(source)
+    
+    perf = performanceMonitor() # outputs to console info on performance - eg what fraction of data was not pulled from live stream and thus missed
+    
+    for data in ds:
+        # performance monitor - frequency of displaying data + loop duration
+        perf.iteration()
+        # Hand Data from datastream to plots and performance monitor
+        # TOF
         x = np.squeeze(data['x_tof']); y = np.squeeze(data['tof'])
         doc.add_next_tick_callback(partial(update_pipe, x=x, y=y))
-        #TrainId
-        trainId_old = trainId
+        # TrainId
         trainId = str(data['tid'])
-        # monitor for loop step duration
-        for_loop_step_dur = time.time()-t_start_loop
-        #Monitor Train ID skipping
-        if n == 0:
-            trainId_old = str(int(trainId) -1)
-        if int(trainId) - int(trainId_old) is not 1:
-            skip_count +=1
         
+        perf.update_trainId(trainId) # give current train id to performance monitor for finding skipping of shots
+        perf.time_for_loop_step() # tell performance monitor that this is the end of the for loop
 
-        
 
-#doc = renderer.server_doc(layout)
-renderer = renderer.instance(mode="server")
-hvplot = renderer.get_plot(layout)  
+# Helper to convert from holoviews to bokeh
+def hv_to_bokeh_obj(hv_layout):
+    hv_plot = renderer.get_plot(hv_layout) 
+    return hv_plot.state
+    
+# plot tools functions
+def largeData_line_plot(pipe, width=1500, height=400,ylim=(-500, 40),xlim=(start_tof,start_tof+N_datapts), xlabel="index", ylabel="TOF signal", cmap = ['blue'], title=None):
+    TOF_dmap = hv.DynamicMap(hv.Curve, streams=[pipe])
+    TOF_dmap_opt = datashade(TOF_dmap, streams=[PlotSize, RangeXY], dynamic=True, cmap = cmap)
+    return hv_to_bokeh_obj( TOF_dmap_opt.opts(width=width,height=height,ylim=ylim,xlim=xlim, xlabel=xlabel, ylabel=ylabel, title = title) )
+    
 
-doc.add_root(hvplot.state)
 
-#doc.title = 'HoloViews App'
+# Data pipes and buffers for plots
+_pipe__TOF_single = Pipe(data=[])
+   
+# SETUP PLOTS
 
-print("Let's make some data")
-#makeBigData()
+# example for coupled plots
+#         layout = hv.Layout(largeData_line_plot(_pipe__TOF_single, title="TOF single shots - LIVE") + largeData_line_plot(_pipe__TOF_single, title="TOF single shots - LIVE 2", cmap=['red'])).cols(1)
+bokeh_live_tof =  largeData_line_plot(_pipe__TOF_single, title="TOF single shots - LIVE") 
+bokeh_live_tof_duplicate = largeData_line_plot(_pipe__TOF_single, title="TOF single shots - LIVE", cmap=["red"])
+
+# SET UP BOKEH LAYOUT
+bokeh_layout = column(bokeh_live_tof,bokeh_live_tof_duplicate)
+
+# add bokeh layout to current doc
+doc.add_root(bokeh_layout)
+
+# Start Thread for Handling of the Live Data Strem
 thread = Thread(target=makeBigData)
 thread.start()
