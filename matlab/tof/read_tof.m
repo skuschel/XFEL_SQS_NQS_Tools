7060424a5b7627075827f50412665e0d07f39103
Lennart Seiffert <lennart@exflonc36.desy.de>
2019-06-05 03:09:39 +0200

    Introduced new folder structure and TOF calibration

---
+++ matlab/tof/read_tof.m
@@ -0,0 +1,17 @@
+function out = read_tof(info)
+    files = get_files(info.path, 'DA02');
+        
+    out.data = [];
+    out.trainId = [];
+    
+    for i=1:numel(files)
+        path_full = sprintf('%s/%s', files(i).folder, files(i).name);
+
+        TOF_data     = '/INSTRUMENT/SQS_DIGITIZER_UTC1/ADC/1:network/digitizers/channel_1_A/raw/samples';
+        TOF_trainId  = '/INSTRUMENT/SQS_DIGITIZER_UTC1/ADC/1:network/digitizers/trainId';
+    
+        out.data   = cat(2, out.data, h5read(path_full, TOF_data));
+        out.trainId = [out.trainId; h5read(path_full, TOF_trainId)]; 
+
+    end
+end
