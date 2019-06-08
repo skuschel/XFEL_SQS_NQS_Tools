097569989ff1d4b1a14be0dc9165765e54915e92
Lennart <lennart.seiffert@uni-rostock.de>
2019-06-09 00:40:50 +0200

    Added database functions 2

--- matlab/database/db_find.m
+++ matlab/database/db_find.m
@@ -4,13 +4,12 @@ function entries = db_find(db, fieldname, value)
     for i=1:numel(db.entry)
         if ischar(value)
             if any(strcmp(db.entry(i).(fieldname), value))
-               %entries = [entries, db.entry(i)];
                entries = [entries, i];
             end
-        else if db.entry(i).(fieldname)==value
-           %entries = [entries, db.entry(i)];
+        else
+            if ~isempty(intersect(db.entry(i).(fieldname), value))
+%            elseif db.entry(i).(fieldname)==value      
            entries = [entries, i];
         end
-        
     end
 end
